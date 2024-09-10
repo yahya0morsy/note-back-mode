a764ea780d86ec6c009331ee0f9c60aa056b88e6
yahya0morsy <ym60307@gmail.com>
2024-09-10 23:15:24 +0300

    fifth-commit

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var app = express();
 
 mongoose.connect('mongodb+srv://h632097:yahya666@cluster0.xv5zm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(console.log("database connect"))
 let corsOptions = {
-  origin: [ 'http://localhost:5173', 'http://localhost:5175' ],
+  origin: [ 'http://localhost:5173', 'http://localhost:5175','https://yahya0morsy.github.io/note-front' ],
   credentials: true 
 };
 app.use(cors(corsOptions));
